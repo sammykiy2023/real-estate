f14eea9af05fede3a6860f4b8d3ab84c7e3e85e5
Samuel Fisher <skiylinedigital@gmail.com>
2024-06-09 03:43:32 -0700

    Create a test api route.

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new mopngoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {type: String,
                 required: true,
                 unique: true,
