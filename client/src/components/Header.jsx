ab9d9a1f327f23accc4e5a7742cba8b5bf3594d1
Samuel Fisher <skiylinedigital@gmail.com>
2024-06-12 21:18:16 -0700

    Completed profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux'; // for authentication
 
 export default function header() {
+
+  const { currentUser } = useSelector(state => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,8 +35,12 @@ export default function header() {
               About
             </li>
           </Link>
-          <Link to="./sign-in">
-            <li className="text-slate-700 hover:underline">Sign In</li>
+          <Link to="./profile">
+            {currentUser ? (
+                <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar}  alt='profile' />
+              ) : (
+                <li className="text-slate-700 hover:underline">Sign In</li>
+              )}
           </Link>
         </ul>
       </div>
