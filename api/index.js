abea6e0da69d79febe1b5f15cdc33f82de13e2ee
Samuel Fisher <skiylinedigital@gmail.com>
2024-06-20 17:37:13 -0700

    Image upload functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -12,8 +13,11 @@ mongoose.connect(process.env.MONGO).then(() => {
     }).catch((err) => {
         console.log(err);
     })
-const app = express();          
+const app = express();    
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
         console.log('Server is running on port 3000!!');
