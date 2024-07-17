f5a1811f4c8f58cef7e617a7d376049280798144
Samuel Fisher <skiylinedigital@gmail.com>
2024-07-16 19:24:00 -0700

    Create User List ing API Route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,28 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import About from './pages/About';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   return (
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route element={<PrivateRoute />}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-      </Route>
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>);
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
